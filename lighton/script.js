ea91edb439de9b16680e6bb64f21a957ed1786b7
Raj Sonar <145009657+Rajsonar25@users.noreply.github.com>
2024-10-27 22:05:00 +0530

    Delete lighton directory

--- lighton/script.js
+++
@@ -1,9 +0,0 @@
-function lightme(show) {
-  var press;
-  if (show == 0) {
-    press = "lightoff.jpg";
-  } else {
-    press = "lighton.jpg";
-  }
-  document.getElementById("light-on").src = press;
-}
